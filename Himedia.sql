c92e1672c5efafe378c0e49b293474b060348161
JJiwonn <163959530+JJiwonn@users.noreply.github.com>
2024-05-10 15:51:24 +0900

    20240510 commit

--- Himedia.sql
+++ Himedia.sql
@@ -1,4 +1,5 @@
 DESC author;
+
 DESC book;
 
 SELECT * FROM author;
@@ -12,3 +13,16 @@ SELECT * FROM author;
 
 -- LIST
 SELECT author_id, author_name, author_desc FROM author;
+
+-- TABLE
+CREATE TABLE PHONE_BOOK (
+    id NUMBER(10) PRIMARY KEY,
+    name VARCHAR2(10),
+    hp VARCHAR2(20),
+    tel VARCHAR2(20)
+);
+DESC PHONE_BOOK;
+SELECT * FROM PHONE_BOOK;
+CREATE SEQUENCE seq_phone_book;
+SELECT * FROM user_objects WHERE OBJECT_TYPE = 'SEQUENCE';
+COMMIT;
